af22a7ae9b70284c39db5730ab93c97e4ffb58b1
CrossNick <vaseninofficial@gmail.com>
2019-08-15 14:32:37 +0000

    Created project

---
+++ SuperheroesApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SuperheroesApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
